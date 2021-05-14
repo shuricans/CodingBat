fe155a7361e5494a50edfdac00431b775d48f217
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 16:31:38 +0300

    Warmup-1 > notString

---
+++ src/main/java/com/codingbat/shuricans/notString/Solution.java
@@ -0,0 +1,17 @@
+package com.codingbat.shuricans.notString;
+
+/**
+ * Warmup-1 > notString
+ * https://codingbat.com/prob/p191914
+ *
+ *
+ * Given a string, return a new string where "not " has been added to the front.
+ * However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+ */
+
+public class Solution {
+    public String notString(String str) {
+        return str.startsWith("not") ? str : "not " + str;
+    }
+
+}
