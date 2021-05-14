74caf1437c874bfaf5e6a67f5a2f97ff2ddf1b00
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 16:27:44 +0300

    Warmup-1 > posNeg

---
+++ src/main/java/com/codingbat/shuricans/posNeg/Solution.java
@@ -0,0 +1,17 @@
+package com.codingbat.shuricans.posNeg;
+
+
+/**
+ * Warmup-1 > posNeg
+ * https://codingbat.com/prob/p159227
+ *
+ *
+ * Given 2 int values, return true if one is negative and one is positive.
+ * Except if the parameter "negative" is true, then return true only if both are negative.
+ */
+
+public class Solution {
+    public boolean posNeg(int a, int b, boolean negative) {
+        return (negative && a < 0 && b < 0) || !negative && ((a < 0 && b > 0) || (a > 0 && b < 0));
+    }
+}
