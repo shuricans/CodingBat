569e43855984f15a21cb535e90d93094b0df9494
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 17:21:40 +0300

    Warmup-1 > backAround

---
+++ src/main/java/com/codingbat/shuricans/backAround/Solution.java
@@ -0,0 +1,17 @@
+package com.codingbat.shuricans.backAround;
+
+/**
+ * Warmup-1 > backAround
+ * https://codingbat.com/prob/p161642
+ *
+ *
+ * Given a string, take the last char and return a new string with the last char added at the front and back,
+ * so "cat" yields "tcatt". The original string will be length 1 or more.
+ */
+
+public class Solution {
+
+    public String backAround(String str) {
+        return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
+    }
+}
---
+++ src/test/java/com/codingbat/shuricans/backAround/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.codingbat.shuricans.backAround;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    /*
+    backAround("cat") → "tcatt"
+    backAround("Hello") → "oHelloo"
+    backAround("a") → "aaa"
+     */
+    @Test
+    public void testBackAround() {
+        assertEquals("tcatt", solution.backAround("cat"));
+        assertEquals("oHelloo", solution.backAround("Hello"));
+        assertEquals("aaa", solution.backAround("a"));
+    }
+}
