fa162fdd182297c4b76e2d8d895fbc6cadb41d2a
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 18:14:39 +0300

    Warmup-1 > in1020

---
+++ src/main/java/com/codingbat/shuricans/in1020/Solution.java
@@ -0,0 +1,14 @@
+package com.codingbat.shuricans.in1020;
+
+/**
+ * Warmup-1 > in1020
+ * https://codingbat.com/prob/p144535
+ *
+ * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+ */
+public class Solution {
+
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+    }
+}
