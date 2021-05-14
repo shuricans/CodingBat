deb45ecc257039a9d2eea944d0e8536f52d862a0
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 18:07:01 +0300

    Warmup-1 > startHi

---
+++ src/main/java/com/codingbat/shuricans/startHi/Solution.java
@@ -0,0 +1,15 @@
+package com.codingbat.shuricans.startHi;
+
+/**
+ * Warmup-1 > startHi
+ * https://codingbat.com/prob/p191022
+ *
+ * Given a string, return true if the string starts with "hi" and false otherwise.
+ */
+
+public class Solution {
+
+    public boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+}
