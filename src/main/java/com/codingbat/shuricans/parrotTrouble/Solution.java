cc56f8e6d46223fce79a3d576218c2be6ff8fe4c
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 16:01:22 +0300

    Warmup-1 > parrotTrouble

---
+++ src/main/java/com/codingbat/shuricans/parrotTrouble/Solution.java
@@ -0,0 +1,18 @@
+package com.codingbat.shuricans.parrotTrouble;
+
+/**
+ * Warmup-1 > parrotTrouble
+ * https://codingbat.com/prob/p140449
+ *
+ * We have a loud talking parrot.
+ * The "hour" parameter is the current hour time in the range 0..23.
+ * We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+ * Return true if we are in trouble.
+ */
+
+public class Solution {
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return talking && (hour < 7 || hour > 20);
+    }
+}
---
+++ src/test/java/com/codingbat/shuricans/parrotTrouble/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.codingbat.shuricans.parrotTrouble;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    private final Solution solution = new Solution();
+
+    /*
+    parrotTrouble(true, 6) → true
+    parrotTrouble(true, 7) → false
+    parrotTrouble(false, 6) → false
+     */
+
+    @Test
+    public void testParrotTrouble() {
+        assertTrue(solution.parrotTrouble(true, 6));
+        assertFalse(solution.parrotTrouble(true, 7));
+        assertFalse(solution.parrotTrouble(false, 6));
+    }
+}
