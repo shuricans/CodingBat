bf265a3a4d74603955ff192afc3e1a59b547e5da
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-14 18:11:51 +0300

    Warmup-1 > icyHot

---
+++ src/main/java/com/codingbat/shuricans/icyHot/Solution.java
@@ -0,0 +1,14 @@
+package com.codingbat.shuricans.icyHot;
+
+/**
+ * Warmup-1 > icyHot
+ * https://codingbat.com/prob/p192082
+ *
+ * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+ */
+public class Solution {
+
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100);
+    }
+}
