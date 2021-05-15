93474826b988478134fce3b9ef19dc7f7377880d
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-15 14:07:25 +0300

    Warmup-2 > frontTimes

---
+++ src/main/java/com/codingbat/shuricans/warmup2/frontTimes/Solution.java
@@ -0,0 +1,30 @@
+package com.codingbat.shuricans.warmup2.frontTimes;
+
+/**
+ * Warmup-2 > frontTimes
+ * https://codingbat.com/prob/p101475
+ *
+ * Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+ * or whatever is there if the string is less than length 3. Return n copies of the front;
+ *
+ *
+ * frontTimes("Chocolate", 2) → "ChoCho"
+ * frontTimes("Chocolate", 3) → "ChoChoCho"
+ * frontTimes("Abc", 3) → "AbcAbcAbc"
+ */
+public class Solution {
+
+    public String frontTimes(String str, int n) {
+        String add;
+        StringBuilder result = new StringBuilder();
+        if(str.length() <= 3) {
+            add = str;
+        } else {
+            add = str.substring(0, 3);
+        }
+        for (int i = 0; i < n; i++) {
+            result.append(add);
+        }
+        return result.toString();
+    }
+}
