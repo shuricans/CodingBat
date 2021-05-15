8101ea599205b8e97f133419ccedae43898a477c
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-05-15 12:30:19 +0300

    Warmup-1 > lastDigit

---
+++ src/main/java/com/codingbat/shuricans/lastDigit/Solution.java
@@ -0,0 +1,15 @@
+package com.codingbat.shuricans.lastDigit;
+
+/**
+ * Warmup-1 > lastDigit
+ * https://codingbat.com/prob/p125339
+ *
+ * Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+ * Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+ */
+public class Solution {
+
+    public boolean lastDigit(int a, int b) {
+        return a % 10 == b % 10;
+    }
+}
