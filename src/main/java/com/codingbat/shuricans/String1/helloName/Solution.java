608c29de437a477479f11ab92fa206c16d2ff8c5
Sasha Karachev <shuricans@gmail.com>
2021-05-17 10:46:43 +0300

    String-1 > helloName

---
+++ src/main/java/com/codingbat/shuricans/String1/helloName/Solution.java
@@ -0,0 +1,19 @@
+package com.codingbat.shuricans.String1.helloName;
+
+/**
+ * String-1 > helloName
+ * https://codingbat.com/prob/p171896
+ *
+ * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+ *
+ *
+ * helloName("Bob") → "Hello Bob!"
+ * helloName("Alice") → "Hello Alice!"
+ * helloName("X") → "Hello X!"
+ */
+public class Solution {
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
